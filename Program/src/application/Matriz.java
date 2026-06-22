e41434b53655b92e3cf3d7270c595e4bded006b4
Matheus <matheuswschneider@gmail.com>
2026-06-22 20:04:48 -0300

    MmMmMm

--- Program/src/application/Matriz.java
+++ Program/src/application/Matriz.java
@@ -6,14 +6,16 @@ public class Matriz {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
-        System.out.println("Digite dois valores (linhas e colunas): ");
+        System.out.println("Digite valor para linhas : ");
         int linhas = sc.nextInt();
+        System.out.println("Digite valor para colunas : ");
         int colunas = sc.nextInt();
 
         int [][] matriz = new int[linhas][colunas];
 
-        for (int i = 0; i < matriz.length; i++) {
-            for(int j =0; j<matriz[i].length; i++){
+        for (int i = 0; i < linhas; i++) {
+            for(int j =0; j<colunas; i++){
+                System.out.println("Digite numeros para: "+i +":"+j);
                 matriz[i][j] = sc.nextInt();
             }
         }
@@ -23,9 +25,15 @@ public class Matriz {
         for (int i = 0; i < matriz.length; i++) {
             for(int j =0; j<matriz[i].length; i++){
                 if (x == matriz[i][j]) {
-                    System.out.println("Position:"+matriz[i]+matriz[j]);
+                    System.out.println("Posição:"+matriz[i]+matriz[j]);
+                    System.out.println("Esquerda::"+matriz[i][j-1]);
+                    System.out.println("Direita:"+matriz[i][j+1]);
+                    System.out.println("Cima:"+matriz[i-1][j]);
+                    System.out.println("baixo:"+matriz[i+1][j]);
+                    System.out.println();
                 }
             }
         }
+        sc.close();
     }
 }
